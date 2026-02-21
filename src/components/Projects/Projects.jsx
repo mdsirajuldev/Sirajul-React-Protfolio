0dc5382558e13d779d208a69846ce6c672f6d1e7
Md Sirajul Islam <mdsirajulislam.dev@gmail.com>
2026-02-21 17:29:43 +0600

    Update project image for Restaurant Website in Projects component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -67,7 +67,7 @@ const projectData = [
   {
     title: "Restaurant Website",
     description: "Restaurant landing page built using wix studio.",
-    image: Project17,
+    image: Project14,
     tags: ["CMS, Wix Studio"],
     url: "https://websolutionstech.wixstudio.com/italian-food",
   },
